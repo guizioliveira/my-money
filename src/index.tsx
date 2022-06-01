80b3eeda26e3fa28dc9523c27a4d586ea1819f31
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-06-01 19:23:53 -0300

    feat(components): add and use `MirageJS`

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { createServer } from "miragejs";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+createServer({
+  routes() {
+    this.namespace = "/api";
+
+    this.get("/transactions", () => [
+      {
+        title: "New transiction",
+        value: 500,
+        category: "Food",
+        date: "02/20/2022"
+      }
+    ]);
+  }
+});
+
 root.render(
   <React.StrictMode>
     <App />
